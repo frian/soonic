18c8cfd8578ff8b0d095df9a92af6048f1e26d4f
frian <a@frian.org>
2019-04-15 16:35:08 +0200

    implemented play song from songslist

--- web/js/player.js
+++ web/js/player.js
@@ -1,9 +1,18 @@
 $(function() {
-    console.log("loaded player");
 
     var playerStatus = "paused";
     var statusClass = '';
 
+
+    // var audio = $("#player")[0];
+    // $("#player").on("loadedmetadata", function() {
+    //     alert(audio.duration);
+    // });
+
+
+    /**
+     * Play / Pause currently loaded song
+     */
     $(document).on("click", ".icon-play, .icon-pause", function(e) {
 
         var player = document.getElementById("player");
@@ -21,4 +30,63 @@ $(function() {
 
         $(this).attr("class", statusClass);
     });
+
+
+    /**
+     * load and play a song from the songs list
+     */
+     $(document).on("click", "#songslist tbody tr", function(e) {
+
+         var path = $(this).data("path");
+
+         var buff = path.split('/');
+         path = '/' + buff.slice(4).join('/');
+
+         console.log(path);
+
+         var format = $(this).data("format");
+
+         var values = $(this).find('td').map(function() {
+             return $(this).text();
+         }).get();
+
+         var artist = values[1];
+         var title = values[2];
+         var duration = values[4];
+
+         var mpegSource = document.getElementById("mpegSource");
+
+         $(mpegSource).attr('src', path);
+
+         var player = document.getElementById("player");
+
+         player.load();
+
+         $(".songInfo").html(title + ' - ' + artist);
+
+         player.play();
+         playerStatus = "playing";
+
+         if ($("#startStopButton").attr("class") === 'icon-play') {
+             $("#startStopButton").attr("class", "icon-pause");
+         }
+
+         console.log($("#startStopButton").attr("class"));
+     });
+
+
 });
+
+/**
+ * seconds to minutes:seconds
+ */
+function formatDuration(rawDuration) {
+
+    rawDuration = Math.round(rawDuration);
+    var durationSeconds = parseInt(rawDuration % 60);
+    var durationMinutes = parseInt(rawDuration / 60) % 60;
+
+    durationSeconds = durationSeconds < 10 ? '0' + durationSeconds : durationSeconds ;
+
+    return durationMinutes + ':' + durationSeconds;
+}
