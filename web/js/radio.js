4028e4606918f1cba82ffce230370908e9902a30
frian <a@frian.org>
2019-04-28 17:07:33 +0200

    working radio page

--- web/js/radio.js
+++ web/js/radio.js
@@ -1,5 +1,4 @@
 $(function() {
-    console.log("loaded radio.js");
 
     var playerStatus = "paused";
 
@@ -8,21 +7,43 @@ $(function() {
      */
     $(document).on("click", ".icon-play, .icon-pause", function(e) {
 
+console.log($(this).parent().parent()[0]);
+
+$(this).parent().parent().addClass("activeRadio");
+
+        // -- find currently active player and pause it
+
+        var activePlayerButton = $("i.activePlayer")[0];
+
+        var activePlayer = null;;
+        if (activePlayerButton) {
+            activePlayer = $(activePlayerButton).next()[0];
+            activePlayer.pause();
+            $(activePlayerButton).removeClass("icon-pause");
+            $(activePlayerButton).addClass("icon-play");
+            $(activePlayerButton).removeClass("activePlayer");
+            playerStatus = "paused";
+        }
+
         var player = $(this).next()[0];
+
+        if (player == activePlayer) {
+            playerStatus = "playing";
+        }
+
 
         if (playerStatus === "paused") {
             player.play();
             playerStatus = "playing";
             $(this).removeClass("icon-play");
             $(this).addClass("icon-pause");
-            $(this).addClass("active");
+            $(this).addClass("activePlayer");
         } else {
             player.pause();
             playerStatus = "paused";
             $(this).removeClass("icon-pause");
             $(this).addClass("icon-play");
-            $(this).removeClass("active");
+            $(this).removeClass("activePlayer");
         }
-
     });
 });
