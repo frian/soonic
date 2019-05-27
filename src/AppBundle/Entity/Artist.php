16827036514ac9bda14d38e7d1d7cdcdce2848a9
frian <a@frian.org>
2019-05-27 18:35:18 +0200

    cleaned Artist Entity

--- src/AppBundle/Entity/Artist.php
+++ src/AppBundle/Entity/Artist.php
@@ -7,53 +7,11 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * Artist
  *
- * @ORM\Table(name="artist", indexes={@ORM\Index(name="idx_artist_name", columns={"name"}), @ORM\Index(name="idx_artist_present", columns={"present"})})
+ * @ORM\Table(name="artist", indexes={@ORM\Index(name="idx_artist_name", columns={"name"})})
  * @ORM\Entity(repositoryClass="AppBundle\Repository\ArtistRepository")
  */
 class Artist
 {
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="name", type="string", length=256, nullable=false)
-     */
-    private $name;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="cover_art_path", type="string", length=1024, nullable=true)
-     */
-    private $coverArtPath;
-
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="album_count", type="integer", nullable=true)
-     */
-    private $albumCount = '0';
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="last_scanned", type="datetime", nullable=true)
-     */
-    private $lastScanned;
-
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="present", type="boolean", nullable=true)
-     */
-    private $present;
-
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="folder_id", type="integer", nullable=true)
-     */
-    private $folderId;
-
     /**
      * @var integer
      *
@@ -63,7 +21,37 @@ class Artist
      */
     private $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=256, nullable=false)
+     */
+    private $name;
 
+    /**
+    * @var integer
+    *
+    * @ORM\Column(name="album_count", type="integer", nullable=true)
+    */
+    private $albumCount = '0';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cover_art_path", type="string", length=1024, nullable=true)
+     */
+    private $coverArtPath;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
     /**
      * Set name
@@ -87,30 +75,6 @@ class Artist
     public function getName()
     {
         return $this->name;
-    }
-
-    /**
-     * Set coverArtPath
-     *
-     * @param string $coverArtPath
-     *
-     * @return Artist
-     */
-    public function setCoverArtPath($coverArtPath)
-    {
-        $this->coverArtPath = $coverArtPath;
-
-        return $this;
-    }
-
-    /**
-     * Get coverArtPath
-     *
-     * @return string
-     */
-    public function getCoverArtPath()
-    {
-        return $this->coverArtPath;
     }
 
     /**
@@ -138,84 +102,26 @@ class Artist
     }
 
     /**
-     * Set lastScanned
+     * Set coverArtPath
      *
-     * @param \DateTime $lastScanned
+     * @param string $coverArtPath
      *
      * @return Artist
      */
-    public function setLastScanned($lastScanned)
+    public function setCoverArtPath($coverArtPath)
     {
-        $this->lastScanned = $lastScanned;
+        $this->coverArtPath = $coverArtPath;
 
         return $this;
     }
 
     /**
-     * Get lastScanned
+     * Get coverArtPath
      *
-     * @return \DateTime
+     * @return string
      */
-    public function getLastScanned()
+    public function getCoverArtPath()
     {
-        return $this->lastScanned;
-    }
-
-    /**
-     * Set present
-     *
-     * @param boolean $present
-     *
-     * @return Artist
-     */
-    public function setPresent($present)
-    {
-        $this->present = $present;
-
-        return $this;
-    }
-
-    /**
-     * Get present
-     *
-     * @return boolean
-     */
-    public function getPresent()
-    {
-        return $this->present;
-    }
-
-    /**
-     * Set folderId
-     *
-     * @param integer $folderId
-     *
-     * @return Artist
-     */
-    public function setFolderId($folderId)
-    {
-        $this->folderId = $folderId;
-
-        return $this;
-    }
-
-    /**
-     * Get folderId
-     *
-     * @return integer
-     */
-    public function getFolderId()
-    {
-        return $this->folderId;
-    }
-
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
+        return $this->coverArtPath;
     }
 }
