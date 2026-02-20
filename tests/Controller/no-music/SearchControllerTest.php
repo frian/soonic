bf2444b26b8f0570e28dfae4094cc964583a0933
frian <a@frian.org>
2026-02-20 17:25:02 +0100

    Add short and blank keyword cases to SearchControllerTest

--- tests/Controller/no-music/SearchControllerTest.php
+++ tests/Controller/no-music/SearchControllerTest.php
@@ -30,4 +30,36 @@ class SearchControllerTest extends NoMusicWebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSame(1, $crawler->filter('body:contains("no songs found")')->count());
     }
+
+    public function testSearchWithTooShortKeywordReturnsForm(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/search');
+
+        $form = $crawler->filter('#search-form')->form([
+            'search[keyword]' => 'ab',
+        ]);
+
+        $crawler = $client->submit($form);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('#search-form');
+        $this->assertSelectorNotExists('i.icon-plus');
+    }
+
+    public function testSearchWithTrimmedEmptyKeywordReturnsForm(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/search');
+
+        $form = $crawler->filter('#search-form')->form([
+            'search[keyword]' => '   ',
+        ]);
+
+        $crawler = $client->submit($form);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('#search-form');
+        $this->assertSelectorNotExists('i.icon-plus');
+    }
 }
