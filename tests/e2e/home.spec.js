fde5b7a0c0264d1f91f92ef78798d0e0aefb8f3d
frian <a@frian.org>
2026-05-14 14:30:15 +0200

    Add AJAX title and history navigation e2e checks

--- tests/e2e/home.spec.js
+++ tests/e2e/home.spec.js
@@ -28,3 +28,60 @@ test('main pages load without JavaScript errors', async ({ page }) => {
 
     expect(errors).toEqual([]);
 });
+
+test('ajax navigation updates document title from visible view metadata', async ({ page }) => {
+    await page.goto('/');
+
+    await assertTitleMatchesView(page, '.library-view [data-page-title]');
+
+    await page.locator('#albums-button').click();
+    await expect(page.locator('.albums-view')).toBeVisible();
+    await assertTitleMatchesView(page, '.albums-view');
+
+    await page.locator('#library-button').click();
+    await expect(page.locator('.library-view')).toBeVisible();
+    await assertTitleMatchesView(page, '.library-view [data-page-title]');
+
+    await page.locator('#radio-button').click();
+    await expect(page.locator('.radios-view')).toBeVisible();
+    await assertTitleMatchesView(page, '.radios-view');
+
+    await page.locator('#settings-button').click();
+    await expect(page.locator('.settings-view')).toBeVisible();
+    await assertTitleMatchesView(page, '.settings-view');
+});
+
+test('ajax history back and forward keep URL and document title in sync', async ({ page }) => {
+    await page.goto('/');
+    await assertTitleMatchesView(page, '.library-view [data-page-title]');
+
+    await page.locator('#albums-button').click();
+    await expect(page).toHaveURL(/\/album\/$/);
+    await assertTitleMatchesView(page, '.albums-view');
+
+    await page.locator('#radio-button').click();
+    await expect(page).toHaveURL(/\/radio\/$/);
+    await assertTitleMatchesView(page, '.radios-view');
+
+    await page.goBack();
+    await expect(page).toHaveURL(/\/album\/$/);
+    await assertTitleMatchesView(page, '.albums-view');
+
+    await page.goBack();
+    await expect(page).toHaveURL(/\/$/);
+    await assertTitleMatchesView(page, '.library-view [data-page-title]');
+
+    await page.goForward();
+    await expect(page).toHaveURL(/\/album\/$/);
+    await assertTitleMatchesView(page, '.albums-view');
+
+    await page.goForward();
+    await expect(page).toHaveURL(/\/radio\/$/);
+    await assertTitleMatchesView(page, '.radios-view');
+});
+
+async function assertTitleMatchesView(page, selector) {
+    const expectedTitle = await page.locator(selector).first().getAttribute('data-page-title');
+    expect(expectedTitle).toBeTruthy();
+    await expect(page).toHaveTitle(expectedTitle);
+}
